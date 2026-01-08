1c8c11837b92fd71ee73a6cdc94a11e7abbe8ec5
zhenmaqiang <127826272+zhenmaqiang@users.noreply.github.com>
2026-01-08 13:34:03 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/<abacus-website>/',
   server: {
     host: '0.0.0.0',
-    port: 3000
-  }
+    port: 3000,
+  },
 })
+
